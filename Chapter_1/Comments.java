ddad79bdca92e826ba42c90cb61bd8e58cc04ae7
JanheaMaeAntipuesto-UCM <janheaantipuesto@gmail.com>
2025-09-16 09:01:08 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+   public static void main(String[] args){
+      //This is a a single-line comment
+      
+      System.out.println("Hello, this is a Java Program with comments!");
+      
+      /*This is a multi-line comment.
+         You can write notes here that take up more than one line
+       */
+       
+      System.out.println("Multi-line comments are useful for explanations.");
+      
+      /**This is a documentation comment.
+         *It is usually to describe classes, methods, or variables.
+      */
+      
+      System.out.println("Documentation comments are used for genereating docs.");
+  }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+   public static void main (String[] args){
+   System.out.println("Zip Up My Fly by Malcolm Todd: I wanna love you and tell you that you are mine");
+   System.out.println("You told me you think that I smell good, then told me zip up my fly");
+   System.out.println("I wanna kiss you in the back of my car that I barely drive");
+   System.out.println("Because I share it with my mom and my mom's busy all the time, I tell ya");
+   }
+}
